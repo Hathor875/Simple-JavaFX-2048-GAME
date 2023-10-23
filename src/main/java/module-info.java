71068e3318a71d5ecabe3df56ce89086eb9fa20b
Hathor875 <krzysztofcieslik875@gmail.com>
2023-10-23 22:05:39 +0200

    WARNING: Unsupported JavaFX configuration: classes were loaded from 'unnamed module @1c3fbdc9'
    add working color
    todo gameover win and score
    fix warning

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,7 @@
 module com.example.simple2048game {
+    requires javafx.graphics;
 
 
-            
-                            
     opens com.example.simple2048game to javafx.fxml;
     exports com.example.simple2048game;
 }
